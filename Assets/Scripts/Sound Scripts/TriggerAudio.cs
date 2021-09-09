33425b9226a12b891a44866a6b3ea3fa8131cb00
TottiDev <manoloduquea1@gmail.com>
2021-09-09 02:59:48 -0500

    Fmod package y algunas implementaciones
    Sonido de botones y música principal

---
+++ Assets/Scripts/Sound Scripts/TriggerAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Event;
+    public bool PlayOnAwake;
+
+    public void PlayOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+    }
+
+
+    private void Start()
+    {
+        if (PlayOnAwake)
+            PlayOneShot();
+    }
+
+}

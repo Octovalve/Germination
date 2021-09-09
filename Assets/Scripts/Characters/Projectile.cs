9dabeefe47debad3776cba44ef950c13effb5d0f
Camilo Sánchez <53084632+cmilo1357@users.noreply.github.com>
2021-09-09 10:21:35 -0500

    Start

---
+++ Assets/Scripts/Characters/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] float launchForce;
+    [SerializeField] Transform shotPoint;
+
+    void Update()
+    {
+        Vector2 weaponPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - weaponPosition;
+    }
+
+    void Shoot()
+    {
+        GameObject newProjectile = Instantiate(projectile, shotPoint.position, shotPoint.rotation);
+        newProjectile.GetComponent<Rigidbody>().velocity = transform.right * launchForce;
+    }
+}

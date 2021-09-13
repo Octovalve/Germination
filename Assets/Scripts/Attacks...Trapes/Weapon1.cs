003a7aafb6fb621992e4e8074a5251a3aca73231
Jesus Antonio <jesus.buitrago@parqueexplora.org>
2021-09-13 00:19:37 -0500

    Camara Ataque Y Arma1 completas funcionales

---
+++ Assets/Scripts/Attacks...Trapes/Weapon1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon1 : MonoBehaviour
+{
+    [SerializeField] float ofsetRot;
+    [SerializeField] Transform SpawnP;
+    [SerializeField] GameObject projectile;
+    Vector3 mouseUpPos;
+    Vector3 force;
+    Rigidbody rb;
+    Camera cam;
+    float forceMod = 2;
+    private bool isShoot = false;
+
+    public bool IsShoot { get => isShoot; set => isShoot = value; }
+
+    private void Start()
+    {
+        rb = projectile.GetComponent<Rigidbody>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+    void Update()
+    {
+        Vector3 diference = transform.position - WorldPosition(0);
+        force = SpawnP.position - WorldPosition(0);
+        float rotZ = Mathf.Atan2(-diference.y, -diference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ + ofsetRot);
+        if (Input.GetMouseButton(0))
+        {
+            if (IsShoot == false)
+            {
+                Debug.Log("Mass: " + rb.mass);
+                Trajectory.Instance.UpdateTrajectoryInpuls(forceVector: force * forceMod, rb, startingPoint: SpawnP.position);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Shoot(force);
+        }
+
+    }
+    void Shoot(Vector3 Force)
+    {
+        if (IsShoot) { return; }
+        Trajectory.Instance.HideLine();
+        GameObject bullet = Instantiate(projectile, SpawnP.position, Quaternion.identity) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(force * forceMod, ForceMode.Impulse);
+        IsShoot = true;
+    }
+    private Vector3 WorldPosition(float z)
+    {
+        Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        ground.Raycast(mousePos, out distance);
+        return mousePos.GetPoint(distance);
+    }
+}
--- Assets/Scripts/Characters/Attack.cs
+++ Assets/Scripts/Characters/Attack.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    [SerializeField] Rigidbody weapon1;
-    [SerializeField] Transform SpawnPoint;
-    public void Attack1() { Instantiate(weapon1, SpawnPoint.position, SpawnPoint.rotation); }
-    public void Attack2() { }
-    public void Attack3() { }
+    [SerializeField] GameObject weapon1;
+    [SerializeField] GameObject weapon2;
+    [SerializeField] GameObject weapon3;
+    [SerializeField] Weapon1 w1;
+    TurnControl turnControl;
+    private void Start()
+    {
+        turnControl = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<TurnControl>();
+    }
+    private void Update()
+    {
+        if (turnControl.Estado >= 6)
+        {
+            weapon1.SetActive(false);
+        }
+    }
+    public void Attack1()
+    {
+        w1.IsShoot = false;
+        weapon1.SetActive(true);
+    }
+    public void Attack2()
+    {
+        weapon2.SetActive(true);
+        if (turnControl.Estado >= 6)
+        {
+            weapon2.SetActive(false);
+        }
+    }
+    public void Attack3()
+    {
+        weapon3.SetActive(true);
+        if (turnControl.Estado >= 6)
+        {
+            weapon3.SetActive(false);
+        }
+    }
+    //Instantiate(weapon1, SpawnPoint.position, SpawnPoint.rotation);
 }

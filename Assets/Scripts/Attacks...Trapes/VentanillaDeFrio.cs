6dbf95ad3c2c1f6c3a0326d6e159f91748f685b8
TottiDev <manoloduquea1@gmail.com>
2021-10-11 22:06:44 -0500

    Sonido spike, shoot paredes y sonido hazard congelador

--- Assets/Scripts/Attacks...Trapes/VentanillaDeFrio.cs
+++ Assets/Scripts/Attacks...Trapes/VentanillaDeFrio.cs
@@ -6,10 +6,13 @@ public class VentanillaDeFrio : MonoBehaviour
 {
     [SerializeField] float damageToDeal;
     HP hpScript;
+    [FMODUnity.EventRef]
+    public string FreezeSound;
     //envia el daño al jugador (por alguna razon el daño se enbia 2 veces)
     private void OnTriggerEnter(Collider other)
     {
         hpScript = other.transform.GetComponent<HP>();
+        FMODUnity.RuntimeManager.PlayOneShotAttached(FreezeSound, gameObject);
         hpScript.TackeDamage(damageToDeal);
     }
 }

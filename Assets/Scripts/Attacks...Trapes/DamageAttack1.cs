ccd7273200b4cf1a4d85b7324db43f38a0c2eb6c
Camilo Sánchez <53084632+cmilo1357@users.noreply.github.com>
2021-11-10 16:00:06 -0500

    Shotgun And Weapon Prefabs

---
+++ Assets/Scripts/Attacks...Trapes/DamageAttack1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageAttack1 : MonoBehaviour
+{
+    [SerializeField] float damageToDeal;
+    CameraControl cameracontrol;
+    Transform bulletTransform;
+    [SerializeField] GameObject hitVFX;
+    TurnControl turnControl;
+    Rigidbody rb;
+    HP hpScript;
+    [FMODUnity.EventRef]
+    public string floorCol;
+    [FMODUnity.EventRef]
+    public string wallCol;
+    [FMODUnity.EventRef]
+    public string hitPlayer;
+    private void Start()
+    {
+        turnControl = GameObject.FindGameObjectWithTag("MainCinemachineCamera").GetComponent<TurnControl>();
+        cameracontrol = GameObject.FindGameObjectWithTag("MainCinemachineCamera").GetComponent<CameraControl>();
+        rb = GetComponent<Rigidbody>();
+        bulletTransform = GetComponentInChildren<Transform>();
+    }
+    private void Update()
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        bulletTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        cameracontrol.TEspera = 60;
+        
+        //Berifica si golpeo al jugador toma su script de HP y le pasa un balor de daño a recivir
+        if (collision.gameObject.tag == "Player")
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(hitPlayer, gameObject);
+            hpScript = collision.transform.GetComponent<HP>();
+            hpScript.TackeDamage(damageToDeal);
+            GameObject hit = Instantiate(hitVFX, transform.position, Quaternion.identity) as GameObject;
+            
+            Destroy(gameObject);
+        }
+        //Berifica si es una superficie distinta al muro y si si es distinta prosede con la destrucion del proyectil
+        else
+        {
+            if (collision.gameObject.tag == "jumpingWall")
+            {
+                FMODUnity.RuntimeManager.PlayOneShotAttached(wallCol, gameObject);
+                GameObject hit = Instantiate(hitVFX, transform.position, Quaternion.identity) as GameObject;
+            }
+            if (collision.gameObject.tag != "jumpingWall")
+            {
+                FMODUnity.RuntimeManager.PlayOneShotAttached(floorCol, gameObject);
+                GameObject hit = Instantiate(hitVFX, transform.position, Quaternion.identity) as GameObject;
+              
+                Destroy(gameObject);
+            }
+        }
+    }
+}
